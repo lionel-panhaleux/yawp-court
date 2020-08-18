b1349144c0b354cccfc42753381c03047f6bff8c
Michal <m.twarowski1988@gmail.com>
2020-08-18 13:09:00 +0100

    crypt first look

--- src/app.js
+++ src/app.js
@@ -1,36 +1,36 @@
 var gameState = {
     journal: ["hello world"],
-    table: [{card: 100001, position: (0,0)}],
+    table: [{ card: 100001, position: (0, 0) }],
     players: {}
 }
 var focus = new Set()
 
 const scale = (window.innerWidth - 380) / 1024
 var pixi_app = new PIXI.Application(
-    {width: 1024 * scale, height: 576 * scale, antialias: true}
+    { width: 1024 * scale, height: 576 * scale, antialias: true }
 )
-var zoom_app = new PIXI.Application({width: 358, height: 500, antialias: true})
+var zoom_app = new PIXI.Application({ width: 358, height: 500, antialias: true })
 
 canvas = new Vue({
     el: "#canvas",
     mounted: function () {
         this.$el.appendChild(pixi_app.view)
-      }
-    })
+    }
+})
 
 card_zoom = new Vue({
     el: "#card-zoom",
     mounted: function () {
         this.$el.appendChild(zoom_app.view)
-        }
-    })
+    }
+})
 
 messages = new Vue({
     el: "#messages",
     data: {
         message: "",
         history: ["new game started"],
-        send: function(){
+        send: function () {
             this.history.push(this.message)
             this.message = ""
         }
@@ -41,13 +41,22 @@ messages = new Vue({
     }
 })
 
+controls = new Vue({
+    el: "#controls",
+    methods: {
+        addCryptCard: function (event) {
+            addCryptCard();
+        }
+      }
+})
+
 const loader = PIXI.Loader.shared
 loader
-  .add("https://images.krcg.org/yawpcourt.jpg")
-  .add("https://images.krcg.org/francoisvillon.jpg")
-  .add("background", "https://images.krcg.org/background.jpg")
-  .add("menu", "https://images.krcg.org/menu.png")
-  .load(setup)
+    .add("https://images.krcg.org/yawpcourt.jpg")
+    .add("https://images.krcg.org/francoisvillon.jpg")
+    .add("background", "https://images.krcg.org/background.jpg")
+    .add("menu", "https://images.krcg.org/menu.png")
+    .load(setup)
 
 function setup() {
     const background = new PIXI.Sprite(loader.resources["background"].texture)
@@ -59,17 +68,17 @@ function setup() {
     background.on('pointerupoutside', onBackgroundDragEnd)
     background.on('pointermove', onBackgroundDragMove)
     background.clearMenu = function () {
-        for(card of this.children) {
-            if(focus.size && !focus.has(card)){
+        for (card of this.children) {
+            if (focus.size && !focus.has(card)) {
                 card.alpha = 0.5
             }
             else {
                 card.alpha = 1
-                if(focus.size){
+                if (focus.size) {
                     continue
                 }
             }
-            for(menu of card.children) {
+            for (menu of card.children) {
                 menu.visible = false
             }
         }
@@ -81,7 +90,7 @@ function setup() {
     addCard("https://images.krcg.org/francoisvillon.jpg")
 }
 
-function addCard(name){
+function addCard(name) {
     const card = new PIXI.Sprite(loader.resources[name].texture)
     card.card = name
     card.scale.set(0.2, 0.2)
@@ -111,6 +120,39 @@ function addCard(name){
     pixi_app.stage.getChildAt(0).addChild(card)
 }
 
+function addCryptCard() {
+    const card = new PIXI.Sprite(PIXI.Texture.WHITE)
+    card.card = "https://images.krcg.org/francoisvillon.jpg"
+    card.scale.set(0.2, 0.2)
+    card.anchor.x = 0.5
+    card.anchor.y = 0.5
+    card.x = 140
+    card.y = 184
+    card.width = 71.6
+    card.height = 100
+    card.interactive = true
+    card.buttonMode = true
+    card
+        .on('pointerdown', onCardDragStart)
+        .on('pointerup', onCardDragEnd)
+        .on('pointerupoutside', onCardDragEnd)
+        .on('pointermove', onCardDragMove)
+        .on('pointertap', onCardTap)
+        .on('pointerover', onCardOver)
+        .on('pointerout', onCardEndOver)
+    const menu = new PIXI.Sprite(loader.resources["menu"].texture)
+    menu.anchor.x = 0.5
+    menu.anchor.y = 0.5
+    menu.scale.set(5, 5)
+    menu.interactive = true
+    menu.buttonMode = true
+    menu.visible = false
+    menu.on('pointertap', onMenuTap)
+    card.addChild(menu)
+    pixi_app.stage.getChildAt(0).addChild(card)
+}
+
+
 function onBackgroundDragStart(event) {
     focus.clear()
     this.clearMenu()
@@ -129,42 +171,41 @@ function onBackgroundDragEnd(event) {
 }
 
 function onBackgroundDragMove(event) {
-    if (!this.dragging) {return}
-    if (!this.dragged && Date.now() - this.start > 200){
+    if (!this.dragging) { return }
+    if (!this.dragged && Date.now() - this.start > 200) {
         this.dragged = true
     }
     // draw a red selection rectangle
     const newPosition = this.data.getLocalPosition(this.parent)
     // sort uses lexicographical order by default, we need numeric order
-    x = [this.selection_start.x, newPosition.x].sort((a,b) => a - b)
-    y = [this.selection_start.y, newPosition.y].sort((a,b) => a - b)
+    x = [this.selection_start.x, newPosition.x].sort((a, b) => a - b)
+    y = [this.selection_start.y, newPosition.y].sort((a, b) => a - b)
     var graphics = pixi_app.stage.getChildAt(1)
     graphics.clear()
     graphics.lineStyle(2, 0xFF3300, 1)
     graphics.drawRect(x[0], y[0], x[1] - x[0], y[1] - y[0])
     // put selected cards in focus, remove cards outside of the selection
-    for (var card of pixi_app.stage.getChildAt(0).children){
+    for (var card of pixi_app.stage.getChildAt(0).children) {
         const bounds = card.getBounds()
         if (
-            bounds.x < x[1] && 
-            bounds.x + bounds.width >= x[0] && 
-            bounds.y < y[1] && 
+            bounds.x < x[1] &&
+            bounds.x + bounds.width >= x[0] &&
+            bounds.y < y[1] &&
             bounds.y + bounds.height >= y[0]
-        )
-        {
+        ) {
             focus.add(card)
             card.alpha = 1
         }
-        else{
+        else {
             focus.delete(card)
             card.alpha = 0.5
         }
     }
 }
 
-function onCardTap(event){
+function onCardTap(event) {
     // pointertap is sent at then end of an actual drag&drop - filter it
-    if(this.dragged){
+    if (this.dragged) {
         this.dragged = false
         return
     }
@@ -195,7 +236,7 @@ function onCardDragStart(event) {
 function onCardDragEnd(event) {
     this.dragging = false
     this.data = null
-    if(this.dragged){
+    if (this.dragged) {
         focus.clear()
         this.parent.clearMenu()
     }
@@ -208,15 +249,15 @@ function onCardDragMove() {
             newPosition.x - this.selection_start.x,
             newPosition.y - this.selection_start.y
         ]
-        for(card of focus){
+        for (card of focus) {
             card.x += delta[0]
             card.y += delta[1]
         }
-        this.selection_start.x = newPosition.x 
+        this.selection_start.x = newPosition.x
         this.selection_start.y = newPosition.y
         // pointerup-pointerdown is also called in case of a simple tap or click
         // if we are really dragging, note it and put the card on the foreground
-        if(!this.dragged && Date.now() - this.start > 200){
+        if (!this.dragged && Date.now() - this.start > 200) {
             this.dragged = true
             this.zIndex = Math.max(...this.parent.children.map(c => c.zIndex)) + 0.1
         }
@@ -227,7 +268,7 @@ function onMenuTap(event) {
     // stopping tap propagation also stops the pointerup propagation
     // we need to stop the dragging started on pointerdown manually
     this.parent.dragging = false
-    if(this.parent.angle === 0){
+    if (this.parent.angle === 0) {
         this.parent.angle = 90
         // make sure the menu does not rotate with the card
         this.angle = -90
